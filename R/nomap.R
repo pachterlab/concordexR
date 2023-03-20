03b3dd6a4d306c7d198c367704610a9037c40abf
Kayla Jackson <kaylajac@caltech.edu>
2023-03-20 15:41:14 -0700

    Fix label assignments bug
    Mapping function depends on labels being assigned to rownames, so this needs to be updated with each loop, or else the computation is the same for all iterations. Can do this more efficiently by not updating rownames

--- R/nomap.R
+++ R/nomap.R
@@ -1,23 +1,18 @@
-.calculate_nomap <- function(graph, labels, return.map=FALSE){
+############################
+# Internal functions
+############################
 
-  .check_labels(labels)
-
-  graph <- .set_label_assignments(graph, labels)
+.nomap_trace <- function(graph, labels, return.map=FALSE){
   mapped <- .nomap_map(graph)
 
-  if (return.map) {
-    # not yet implemented, but should return mapped matrix
-    # and nomap statistic
-    mean(diag(mapped))
+  if (return.map){
+    # not yet implemented,
+    return(mean(diag(mapped)))
   }
-
   mean(diag(mapped))
 
 }
 
-############################
-# Internal functions
-############################
 
 #' @importFrom Matrix rowSums t
 .nomap_map <- function(graph){
@@ -31,6 +26,40 @@
   t(out)/rowSums(out)
 }
 
+#' @importFrom BiocParallel SerialParam bplapply
+.calculate_nomap <- function(graph, labels, n.iter=15, return.map=FALSE, BPPARAM=SerialParam()){
+
+  .check_labels(labels)
+
+  graph <- .set_label_assignments(graph, labels)
+  trace <- .nomap_trace(graph, labels)
+
+  # Now shuffle labels and correct the trace
+  trace_random <- bplapply(seq(n.iter), \(x){
+    .nomap_trace(graph, sample(labels))
+  }, BPPARAM=BPPARAM)
+  print(trace_random)
+  trace_random <- mean(unlist(trace_random))
+
+  if (return.map) {
+    # not yet implemented, but should return mapped matrix
+    # and nomap statistics
+    return(
+      list(
+        nomap = trace,
+        mean_random_nomap = trace_random,
+        corrected_trace = trace/trace_random
+      ))
+  }
+
+  list(
+    nomap = trace,
+    mean_random_nomap = trace_random,
+    corrected_trace = trace/trace_random
+  )
+
+}
+
 ############################
 # S4 method definitions
 ############################
@@ -39,6 +68,8 @@
 #' @rdname calculateNomap
 setMethod("calculateNomap", "ANY", .calculate_nomap)
 
+#' @export
+#' #' @rdname calculateNomap
 #' @importFrom SingleCellExperiment reducedDim
 setMethod("calculateNomap", "SingleCellExperiment", function(x, labels,..., graph.name=NULL){
   if (is.null(graph.name)) {
