45ca16932457c6f6064dbc1985c893b1932493a8
Kayla Jackson <kaylajac@caltech.edu>
2023-03-29 15:11:34 -0700

    Add check to graphs

--- R/utils.R
+++ R/utils.R
@@ -4,7 +4,7 @@
   n <- length(uniq)
 
   if (n < 2L){
-    uniq <- character()
+
     cli::cli_abort(
       c("Must have at least 2 class labels",
         "i" = "There {?is/are} {no(n)} class label{?s}"),
@@ -18,8 +18,24 @@
 .set_label_assignments <- function(graph, labels){
 
   dims <- dim(graph)
+
   dimnames(graph) <- list(labels, labels)
 
   graph
 }
 
+#' @importFrom cli cli_abort
+.check_graph <- function(graph){
+  dims <- dim(graph)
+
+  if (length(unique(dims)) != 1L) {
+    cli::cli_abort(
+      c("The dimensions of the graph are not equal",
+        "i" = "There {?is/are} {dims[1]} row{?s}",
+        "i" = "There {?is/are} {dims[2]} column{?s}"),
+      call = rlang::caller_env()
+    )
+  }
+
+  invisible(TRUE)
+}
